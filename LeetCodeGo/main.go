9ce4c9331048044db366991359b38c4044d1eced
Mario Claudius <marioclaudius@Marios-MacBook-Pro.local>
2024-05-13 21:34:51 +0700

    post temporary solution for problem 15

--- LeetCodeGo/main.go
+++ LeetCodeGo/main.go
@@ -7,7 +7,15 @@ import (
 
 func main() {
 	// Problem 1: Two Sum
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	fmt.Println(solution.TwoSum(nums, target))
+	// nums := []int{2, 7, 11, 15}
+	// target := 9
+	// fmt.Println(solution.TwoSum(nums, target))
+
+	// Problem 15: 3Sum
+	// nums1 := []int{-1, 0, 1, 2, -1, -4}
+	// nums2 := []int{0, 0, 0}
+	// nums3 := []int{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0}
+	nums4 := []int{0, -3, 2, -4, -1, -3, -2, 1, -1, 1, 3}
+	// fmt.Println(solution.ThreeSum(nums1)) // should print [[-1,-1,2],[-1,0,1]]
+	fmt.Println(solution.ThreeSum(nums4)) // should print [[-4,1,3],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-2,1,1],[-1,-1,2],[-1,0,1]]
 }
---
+++ LeetCodeGo/problem_solution/problem0015.go
@@ -0,0 +1,69 @@
+package problemsolution
+
+// TIME LIMIT EXCEEDED SOLUTION, USING NORMAL LOOP (308/313 test case passed)
+func ThreeSum(nums []int) [][]int {
+	result := make([][]int, 0)
+	if len(nums) == 3 {
+		sum := 0
+		for i := 0; i < len(nums); i++ {
+			sum += nums[i]
+		}
+
+		if sum == 0 {
+			result = append(result, nums)
+		}
+
+		return result
+	}
+
+	// for edge case when there's 3 zero in array
+	zeroNumCount := 0
+	for a := 0; a < len(nums); a++ {
+		if nums[a] == 0 {
+			zeroNumCount++
+		}
+	}
+
+	if zeroNumCount >= 3 {
+		result = append(result, []int{0, 0, 0})
+	}
+
+	for i := 0; i < len(nums)-2; i++ {
+		firstNum := nums[i]
+		targetSum := -1 * firstNum
+		secondNumMap := make(map[int]int, 0)
+		for j := i + 1; j < len(nums); j++ {
+			currentValue := nums[j]
+			if _, ok := secondNumMap[currentValue]; !ok {
+				secondNumMap[currentValue] = j
+			}
+		}
+
+		for k := i + 2; k < len(nums); k++ {
+			if value, valid := secondNumMap[targetSum-nums[k]]; valid && value != k {
+				solution := []int{firstNum, targetSum - nums[k], nums[k]}
+				isSameSolution := false
+				for _, solutionResult := range result {
+					setMap := make(map[int]bool, 0)
+					for i := 0; i < len(solutionResult); i++ {
+						setMap[solutionResult[i]] = true
+					}
+					isExistOnSet := true
+					for i := 0; i < len(solution); i++ {
+						_, exist := setMap[solution[i]]
+						isExistOnSet = isExistOnSet && exist
+					}
+					if isExistOnSet {
+						isSameSolution = true
+						break
+					}
+				}
+				if !isSameSolution {
+					result = append(result, solution)
+				}
+			}
+		}
+	}
+
+	return result
+}
